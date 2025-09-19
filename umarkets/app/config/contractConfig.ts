4cee9f7de07f1186d84d83e3e8ed9860b6aae31e
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-09-19 14:58:56 +0200

    project setup and contract configs

---
+++ umarkets/app/config/contractConfig.ts
@@ -0,0 +1,6 @@
+export const contractConfig = {
+  marketContractAddress: "0x024Be11C5fDd30a25Ed49c8502d7a0f6DcF44eC3",
+  uZarContractAddress: "0x427271F417A4807b67840FC68d42b1Caabf775eB",
+  chainId: 534351,
+  rpc: "https://sepolia-rpc.scroll.io",
+};
